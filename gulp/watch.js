fd81c5d4a7ba437aad95f289746ba3e4aed554ef
Jason Jacobson <jdesigns.jason@gmail.com>
2017-10-05 13:37:39 -0500

    WIP

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 gulp.task('watch', ['sass'], function () {
+  console.warn('HELLO:::::', process.env.npm_config_env);
     watch('./src/scss/**/*.scss', function(){
       gulp.start('sass');
     });
